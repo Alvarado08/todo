096ae1ac2348dd5cc7512a4771a692a26e98da34
Alvarado08 <jorgealvarado0238@gmail.com>
2023-11-26 18:20:48 -0600

    Project Cards
    Project cards layout with dynamic todos that beong to each project. Finished and unfinished todos color.

--- app.js
+++ app.js
@@ -14,13 +14,15 @@ let uuid = crypto.randomUUID();
 // const { v4: uuidv4 } = require('uuid');
 // console.log(uuidv4());
 
+const projectManagerContainer = document.getElementById('manager-container');
+
 class Todo {
-    constructor(id,title,description,dueDate,priority){
+    constructor(id,title,description,dueDate,status){
         this.id = id;
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
-        this.priority = priority;
+        this.status = status;
     }
     // store = () => {
     //     let todo = {
@@ -28,19 +30,19 @@ class Todo {
     //         title: this.title,
     //         description: this.description,
     //         dueDate: this.dueDate,
-    //         priority: this.priority,
+    //         status: this.status,
     //     }
     //     this.todos.push(todo);
     // }
     // delete = (index) => {
     //     this.todos.splice(index,1);
     // }
-    // edit = (index,title = '',description = '',dueDate='',priority = '') => {
+    // edit = (index,title = '',description = '',dueDate='',status = '') => {
     //     if(index >= 0 && index < this.todos.length){
     //         if(title !== '') this.todos[index].title = title;
     //         if(description !== '') this.todos[index].description = description;
     //         if(dueDate !== '') this.todos[index].dueDate = dueDate;
-    //         if(priority !== '') this.todos[index].priority = priority;
+    //         if(status !== '') this.todos[index].status = status;
     //     }
     // }
     // getTodos = () => {
@@ -59,18 +61,29 @@ class Project {
     delete = (index) => {
         this.todos.splice(index,1);
     }
-    edit = (index,title = '',description = '',dueDate='',priority = '') => {
+    edit = (index,title = '',description = '',dueDate='',status = '') => {
         if(index >= 0 && index < this.todos.length){
             const todoToUpdate = this.todos[index];
             if(title !== '') this.todos[todoToUpdate].title = title;
             if(description !== '') this.todos[todoToUpdate].description = description;
             if(dueDate !== '') this.todos[todoToUpdate].dueDate = dueDate;
-            if(priority !== '') this.todos[todoToUpdate].priority = priority;
+            if(status !== '') this.todos[todoToUpdate].status = status;
         }else{
             console.log('Todo not found!');
         }
     }
     getTodos = () => {
+        const todosContainer = document.querySelectorAll(`.todos-container-${this.title}`);
+        todosContainer.forEach(todoContainer => {
+            this.todos.forEach(todo => {
+                const todoCard = document.createElement('article');
+                todoCard.className = `w-full bg-white rounded p-3 shadow ${todo.status ? 'border-green-500' : 'border-red-500'} border-l-2`;
+                todoCard.innerHTML = `
+                    <h5>${todo.title}</h5>
+                `
+                todoContainer.appendChild(todoCard);
+            })
+        })
         console.log(this.todos);
     }
 }
@@ -86,6 +99,16 @@ class ProjectManager {
         this.projects.splice(index,1);
     }
     getProjects = () => {
+        this.projects.forEach(project => {
+            const projectCard = document.createElement('div');
+            projectCard.className = 'bg-white rounded shadow p-5';
+            projectCard.innerHTML = `
+                <h1 class="font-bold text-lg">${project.title}</h1>
+                <h3><span class="font-semibold">Todos:</span> ${project.todos.length}</h3>
+                <div class="todos-container-${project.title} pt-3 space-y-3"></div>
+            `
+            projectManagerContainer.appendChild(projectCard);
+        })
         console.log(this.projects);
     }
 }
@@ -97,11 +120,13 @@ let project2 = new Project('SPA');
 
 let todo1 = new Todo(uuid,'My Awesome Todo','This is a desc!','2015',true);
 let todo2 = new Todo(uuid,'My Cool Todo','This is a desc x2!','2019',false);
+let todo3 = new Todo(uuid,'My Cool Todo x 2','This is a desc x3!','2019',false);
 
 myProjects.store(project1);
 myProjects.store(project2);
 project1.store(todo1);
 project2.store(todo2);
+project2.store(todo3);
 
 myProjects.getProjects();
 project1.getTodos();
