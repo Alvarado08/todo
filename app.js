7ef2eaacce63634ac4129a4cf104c197c67d859a
Alvarado08 <jorgealvarado0238@gmail.com>
2023-12-02 20:18:43 -0600

    Project Form and Card
    Functional project form adding project cards and add todo button.

--- app.js
+++ app.js
@@ -25,30 +25,6 @@ class Todo {
         this.dueDate = dueDate;
         this.status = status;
     }
-    // store = () => {
-    //     let todo = {
-    //         id: this.id,
-    //         title: this.title,
-    //         description: this.description,
-    //         dueDate: this.dueDate,
-    //         status: this.status,
-    //     }
-    //     this.todos.push(todo);
-    // }
-    // delete = (index) => {
-    //     this.todos.splice(index,1);
-    // }
-    // edit = (index,title = '',description = '',dueDate='',status = '') => {
-    //     if(index >= 0 && index < this.todos.length){
-    //         if(title !== '') this.todos[index].title = title;
-    //         if(description !== '') this.todos[index].description = description;
-    //         if(dueDate !== '') this.todos[index].dueDate = dueDate;
-    //         if(status !== '') this.todos[index].status = status;
-    //     }
-    // }
-    // getTodos = () => {
-    //     return this.todos;
-    // }
 }
 
 class Project {
@@ -88,105 +64,24 @@ class ProjectManager {
     }
     store = (project) => {
         this.projects.push(project);
+        this.getProjects()
     }
     delete = (index) => {
         this.projects.splice(index,1);
     }
     getProjects = () => {
+        while (projectManagerContainer.firstChild) {
+            projectManagerContainer.firstChild.remove();
+        }
         this.projects.forEach(project => {
-            projectCard(project,projectManagerContainer);
+            projectCard(project);
         })
         console.log(this.projects);
     }
 }
-
-function projectCard(project,container) {
-    const projectCard = document.createElement('div');
-    let completed = project.todos.filter(item => item.status);
-    projectCard.className = 'min-h-fit bg-white rounded shadow p-5';
-    projectCard.innerHTML = `
-                <h1 class="font-bold text-lg">${project.title}</h1>
-                <h3><span class="font-semibold">Todos:</span> ${project.todos.length}</h3>
-                <h3 class="${project.todos.length === 0 && 'hidden'}"><span class="font-semibold">Completed:</span> ${completed.length}</h3>
-                <div id="todos-container-${project.title}" class="pt-3 space-y-3"></div>
-            `
-    container.appendChild(projectCard);
-}
+let myProjects = new ProjectManager();
 
 // DOM Single Responsibility Functions
-function todoCard(todo,title) {
-    const todosContainer = document.getElementById(`todos-container-${title}`);
-    const addTodoBtn = document.createElement('button');
-    addTodoBtn.className = 'w-full bg-gray-200/40 text-black/50 border rounded py-3 px-6 duration-300 hover:text-black/80 flex items-center justify-center gap-1';
-    addTodoBtn.innerHTML = `
-            <span>Add task</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" />
-            </svg>
-        `
-    const todoCard = document.createElement('article');
-    todoCard.className = `w-full bg-white rounded p-3 shadow ${todo.status ? 'border-green-500' : 'border-red-500'} border-l-2 flex items-center justify-between duration-300`;
-    todoCard.innerHTML = `
-                <h5 class="hover:cursor-pointer">${todo.title}</h5>
-                <div class="flex items-center gap-1">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="22" height="22" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash text-red-500" width="22" height="22" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                </svg>
-                </div>
-            `
-    todosContainer.appendChild(todoCard);
-    todoCard.addEventListener('click', () => {
-        todo.status = !todo.status;
-        alert(`${todo.status}`)
-    })
-}
-
-function addTodoBtn(title){
-    const todosContainer = document.getElementById(`todos-container-${title}`);
-    const addTodoBtn = document.createElement('button');
-    addTodoBtn.className = 'w-full bg-gray-200/40 text-black/50 border rounded py-3 px-6 duration-300 hover:text-black/80 flex items-center justify-center gap-1';
-    addTodoBtn.innerHTML = `
-            <span>Add task</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" />
-            </svg>
-    `
-    const dialog = document.createElement('dialog');
-    todosContainer.appendChild(addTodoBtn);
-    addTodoBtn.addEventListener("click", () => {
-        dialog.className = 'p-5 rounded bg-white w-full max-w-sm'
-        dialog.innerHTML = `
-                <form class="space-y-3">
-                    <h2 class="font-bold text-2xl">Project: ${title}</h2>
-                    <h3 class="text-xl font-bold">New Todo</h3>
-                    <div>
-                        <label for="title" class="block text-lg font-semibold">Title</label>
-                        <input id="title" type="text" class="w-full rounded p-3 border" placeholder="My awesome todo">
-                    </div>
-                    <div>
-                        <label for="description" class="block text-lg font-semibold">Description</label>
-                        <textarea id="description" class="w-full rounded p-3 border" placeholder="My cool description"></textarea>
-                    </div>
-                    <div>
-                        <label for="date" class="block text-lg font-semibold">Due Date</label>
-                        <input id="date" type="date" class="w-full rounded p-3 border" placeholder="Due date">
-                    </div>
-                    <div class="flex items-center justify-center gap-2">
-                        <button type="submit" class="bg-green-700 rounded py-2 px-4 text-white">Add todo</button>
-                        <button id="close" class="bg-blue-500 rounded py-2 px-4 text-white">Close</button>
-                    </div>
-                </form>
-            `;
-        projectManagerContainer.appendChild(dialog);
-        const closeBtn = document.getElementById('close');
-        dialog.showModal();
-        closeBtn.addEventListener('click', () => {
-            dialog.close();
-            projectManagerContainer.removeChild(dialog);
-        })
-        // alert(`I'm from project ${this.title}`);
-    })
-}
-
 function projectModal(){
     const dialog = document.createElement('dialog');
     dialog.className = 'p-5 rounded bg-white w-full max-w-sm'
@@ -214,8 +109,98 @@ function projectModal(){
     const projectForm = document.getElementById('new-project-form');
     projectForm.addEventListener('submit', (e)=>{
         e.preventDefault();
-        createProject(titleInput);
         dialog.close();
+        projectManagerContainer.removeChild(dialog);
+        createProject(titleInput);
+    })
+}
+
+function projectCard(project) {
+    const projectCard = document.createElement('div');
+    let completed = project.todos.filter(item => item.status);
+    projectCard.className = 'min-h-fit bg-white rounded shadow p-5';
+    projectCard.innerHTML = `
+                <h1 class="font-bold text-lg">${project.title}</h1>
+                <h3><span class="font-semibold">Todos:</span> ${project.todos.length}</h3>
+                <h3 class="${project.todos.length === 0 && 'hidden'}"><span class="font-semibold">Completed:</span> ${completed.length}</h3>
+                <div id="todos-container-${project.title}" class="pt-3 space-y-3"></div>
+            `
+    projectManagerContainer.appendChild(projectCard);
+    const todosContainer = document.getElementById(`todos-container-${project.title}`);
+    addTodoBtn(project.title,todosContainer);
+}
+
+function todoCard(todo,title) {
+    const todosContainer = document.getElementById(`todos-container-${title}`);
+    const todoCard = document.createElement('article');
+    todoCard.className = `w-full bg-white rounded p-3 shadow ${todo.status ? 'border-green-500' : 'border-red-500'} border-l-2 flex items-center justify-between duration-300`;
+    todoCard.innerHTML = `
+                <h5 class="hover:cursor-pointer">${todo.title}</h5>
+                <div class="flex items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="22" height="22" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash text-red-500" width="22" height="22" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg>
+                </div>
+            `
+    todosContainer.appendChild(todoCard);
+    todoCard.addEventListener('click', () => {
+        todo.status = !todo.status;
+        alert(`${todo.status}`)
+    })
+}
+
+function addTodoBtn(title,container){
+    //const todosContainer = document.getElementById(`todos-container-${projectTitle}`);
+    const addTodoBtn = document.createElement('button');
+    addTodoBtn.className = 'w-full bg-gray-200/40 text-black/50 border rounded py-3 px-6 duration-300 hover:text-black/80 flex items-center justify-center gap-1';
+    addTodoBtn.innerHTML = `
+            <span>Add task</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" />
+            </svg>
+    `
+    const dialog = document.createElement('dialog');
+    dialog.className = 'p-5 rounded bg-white w-full max-w-sm';
+    dialog.innerHTML = `
+            <form id="new-todo-form" class="space-y-3">
+                <h2 class="font-bold text-2xl">Project: ${title}</h2>
+                <h3 class="text-xl font-bold">New Todo</h3>
+                <div>
+                    <label for="title" class="block text-lg font-semibold">Title</label>
+                    <input id="title" type="text" class="w-full rounded p-3 border" placeholder="My awesome todo">
+                </div>
+                <div>
+                    <label for="description" class="block text-lg font-semibold">Description</label>
+                    <textarea id="description" class="w-full rounded p-3 border" placeholder="My cool description"></textarea>
+                </div>
+                <div>
+                    <label for="date" class="block text-lg font-semibold">Due Date</label>
+                    <input id="date" type="date" class="w-full rounded p-3 border" placeholder="Due date">
+                </div>
+                <div class="flex items-center justify-center gap-2">
+                    <button type="submit" class="bg-green-700 rounded py-2 px-4 text-white">Add todo</button>
+                    <button id="close" class="bg-blue-500 rounded py-2 px-4 text-white">Close</button>
+                </div>
+            </form>
+        `;
+    container.appendChild(addTodoBtn);
+    addTodoBtn.addEventListener("click", () => {
+        projectManagerContainer.appendChild(dialog);
+        const closeBtn = document.getElementById('close');
+        dialog.showModal();
+        closeBtn.addEventListener('click', () => {
+            dialog.close();
+            projectManagerContainer.removeChild(dialog);
+        })
+        const titleInput = document.getElementById('title');
+        const todoForm = document.getElementById('new-todo-form');
+        todoForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            //createProject(titleInput);
+            dialog.close();
+            projectManagerContainer.removeChild(dialog);
+        })
+        // alert(`I'm from project ${this.title}`);
     })
 }
 
@@ -223,23 +208,27 @@ function createProject(title){
     myProjects.store(new Project(title.value));
 }
 
+function createTodo(){
+
+}
+
 newProjectBtn.addEventListener('click',projectModal);
 
-let myProjects = new ProjectManager();
+// let myProjects = new ProjectManager();
 
-let project1 = new Project('CRM');
-let project2 = new Project('SPA');
+// let project1 = new Project('CRM');
+// let project2 = new Project('SPA');
 
-let todo1 = new Todo(uuid,'My Awesome Todo','This is a desc!','2015',true);
-let todo2 = new Todo(uuid,'My Cool Todo','This is a desc x2!','2019',true);
-let todo3 = new Todo(uuid,'My Cool Todo x 2','This is a desc x3!','2019',false);
+// let todo1 = new Todo(uuid,'My Awesome Todo','This is a desc!','2015',true);
+// let todo2 = new Todo(uuid,'My Cool Todo','This is a desc x2!','2019',true);
+// let todo3 = new Todo(uuid,'My Cool Todo x 2','This is a desc x3!','2019',false);
 
-myProjects.store(project1);
-myProjects.store(project2);
-project1.store(todo1);
-project2.store(todo2);
-project2.store(todo3);
+// myProjects.store(project1);
+// myProjects.store(project2);
+// project1.store(todo1);
+// project2.store(todo2);
+// project2.store(todo3);
 
-myProjects.getProjects();
-project1.getTodos();
-project2.getTodos();
+// myProjects.getProjects();
+// project1.getTodos();
+// project2.getTodos();
